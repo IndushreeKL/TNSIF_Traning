19e436b84299412c759d217a953ac0eca34be8ea
Indushree K L <Indushree K L@DESKTOP-MDJFMQN>
2026-07-29 10:22:49 +0530

    first commit

---
+++ TNSIF_TRAINING/src/org/tnsif/acce/c2tc/MethodOverriding/SuperWithmethod.java
@@ -0,0 +1,33 @@
+package org.tnsif.acce.c2tc.MethodOverriding;
+
+class Payment1 
+{
+	void process()
+	{
+		System.out.println("Processing payment using Standard gateway");
+	}
+}
+
+class Gpay extends Payment1
+{
+	void process()
+	{
+		System.out.println("Processsing payment via Google pay");
+	}
+	
+	void CompleteTransaction()
+	{
+		super.process();
+		process();
+	}
+}
+
+public class SuperWithmethod {
+
+	public static void main(String[] args) {
+		Gpay obj=new Gpay();
+		obj.CompleteTransaction();
+
+	}
+
+}
