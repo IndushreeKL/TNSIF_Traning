199b3f22bd028942c72e7e0f2670c4671b78b623
Indushree K L <Indushree K L@DESKTOP-MDJFMQN>
2026-07-31 10:48:59 +0530

    first commit

---
+++ TNSIF_TRAINING/src/org/tnsif/acce/c2tc/Polymorphism/ConstructorOverloadingDemo.java
@@ -0,0 +1,34 @@
+package org.tnsif.acce.c2tc.Polymorphism;
+
+class Student
+{
+	String name;
+	int age;
+	
+	Student()
+	{
+		System.out.println("Default constructor");
+	}
+	Student(String name)
+	{
+		this.name=name;
+		System.out.println("Name "+name);
+	}
+	Student(String name,int age)
+	{
+		this.name=name;
+		this.age=age;
+		System.out.println("Name "+ name +"Age "+age);	 
+	}	
+}
+public class ConstructorOverloadingDemo {
+
+	public static void main(String[] args) {
+		
+		Student student1 = new Student();
+		Student student2 = new Student("Mili");
+		Student student3 = new Student("Mili",21);
+		
+		}
+		
+	}
