75df33e53926721bae6e625be8665870396595ea
Indushree K L <Indushree K L@DESKTOP-MDJFMQN>
2026-08-05 10:31:34 +0530

    first commit

---
+++ TNSIF_TRAINING/src/org/tnsif/acce/c2tc/scannerclass/bufferreader/AdvancedScannermethod.java
@@ -0,0 +1,10 @@
+package org.tnsif.acce.c2tc.scannerclass.bufferreader;
+
+public class AdvancedScannermethod {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
