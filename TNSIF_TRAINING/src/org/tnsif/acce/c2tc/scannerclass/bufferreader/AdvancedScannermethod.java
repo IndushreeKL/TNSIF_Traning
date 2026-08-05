36f0e21bf3abe65d038781a31bc96155875ca9cc
Indushree K L <Indushree K L@DESKTOP-MDJFMQN>
2026-08-05 10:36:19 +0530

    first commit

--- TNSIF_TRAINING/src/org/tnsif/acce/c2tc/scannerclass/bufferreader/AdvancedScannermethod.java
+++ TNSIF_TRAINING/src/org/tnsif/acce/c2tc/scannerclass/bufferreader/AdvancedScannermethod.java
@@ -1,10 +1,31 @@
 package org.tnsif.acce.c2tc.scannerclass.bufferreader;
 
+import java.util.Scanner;
+
 public class AdvancedScannermethod {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
-	}
+		        Scanner sc = new Scanner(System.in);
+
+		        System.out.print("Enter any value: ");
+
+		        if (sc.hasNext()) {
+		            System.out.println("Input is available.");
+
+		            if (sc.hasNextInt()) {
+		                int num = sc.nextInt();
+		                System.out.println("You entered an integer: " + num);
+		            } else {
+		                String str = sc.next();
+		                System.out.println("You entered a string: " + str);
+		            }
+		        } else {
+		            System.out.println("No input available.");
+		        }
+
+		        sc.close();
+		    }
 
 }
