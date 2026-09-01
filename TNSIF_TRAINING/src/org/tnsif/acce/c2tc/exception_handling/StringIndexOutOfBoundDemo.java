76b4790dfc8b4885f8e6deef871093479dc9d373
Indushree K L <Indushree K L@DESKTOP-MDJFMQN>
2026-09-01 21:38:40 +0530

    first commit

---
+++ TNSIF_TRAINING/src/org/tnsif/acce/c2tc/exception_handling/StringIndexOutOfBoundDemo.java
@@ -0,0 +1,19 @@
+package org.tnsif.acce.c2tc.exception_handling;
+
+public class StringIndexOutOfBoundDemo {
+
+	public static void main(String[] args) {
+		String str="hello";
+		try {
+			char ch=str.charAt(5);
+			System.out.println(ch);
+		}catch(StringIndexOutOfBoundsException e)
+		{
+			System.out.println(e);
+		}
+		
+		
+
+	}
+
+}
