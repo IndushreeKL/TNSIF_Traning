be38bd8f9b34eda6a6307b3796e66fd8e0529264
Indushree K L <Indushree K L@DESKTOP-MDJFMQN>
2026-09-02 10:26:18 +0530

    first commit

---
+++ TNSIF_TRAINING/src/org/tnsif/acce/c2tc/interview_pr_qsns1/PalindromeChecking.java
@@ -0,0 +1,24 @@
+package org.tnsif.acce.c2tc.interview_pr_qsns1;
+import java.util.Scanner;
+
+
+public class PalindromeChecking {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		
+		int rev=0, temp=n;
+		
+		while (n!=0) {
+			rev = rev * 10 + n % 10;
+			n = n / 10;
+	}
+		if (temp == rev)
+			System.out.println("Palindrome");
+		else
+			System.out.println("Not palindrome");
+
+
+    }
+}
