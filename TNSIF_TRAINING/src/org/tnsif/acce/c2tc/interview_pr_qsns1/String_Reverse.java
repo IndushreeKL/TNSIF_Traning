26747c10344c5ffda0dcb3de2dd827ef247520f5
Indushree K L <Indushree K L@DESKTOP-MDJFMQN>
2026-09-02 10:29:11 +0530

    first commit

---
+++ TNSIF_TRAINING/src/org/tnsif/acce/c2tc/interview_pr_qsns1/String_Reverse.java
@@ -0,0 +1,18 @@
+package org.tnsif.acce.c2tc.interview_pr_qsns1;
+import java.util.Scanner;
+
+public class String_Reverse {
+
+	public static void main(String[] args) {
+		
+		 Scanner sc = new Scanner(System.in);
+		 System.out.print("Enter a string: ");
+		 
+		 String str = sc.next();
+
+		 for (int i = str.length() - 1; i >= 0; i--) {
+		 System.out.print(str.charAt(i));
+		 
+		 }
+	}
+}
