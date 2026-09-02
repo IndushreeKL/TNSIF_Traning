46b94d9b50e2509b4bef07e38ddc770251d68597
Indushree K L <Indushree K L@DESKTOP-MDJFMQN>
2026-09-02 11:42:48 +0530

    first commit

---
+++ TNSIF_TRAINING/src/org/tnsif/acce/c2tc/static_final_demo/StaticOnVariable.java
@@ -0,0 +1,30 @@
+package org.tnsif.acce.c2tc.static_final_demo;
+
+
+class Student
+{
+	int id;
+	String name;
+	static String collegename="Cambridge";
+	
+	
+	Student(int i ,String n)
+	{
+		id=i;
+		name=n;
+	}
+	void display() {
+		System.out.println("Id : "+ id +" " +"Name :"+name+" " +"CollegeName : "+collegename);
+	}
+}
+public class StaticOnVariable {
+	public static void main(String[] args) {
+		Student student=new Student(12,"Sushma");
+		Student student1=new Student(13,"Anil");
+		Student.collegename="CITNC";
+        student.display();
+        student1.display();
+
+	}
+
+}
