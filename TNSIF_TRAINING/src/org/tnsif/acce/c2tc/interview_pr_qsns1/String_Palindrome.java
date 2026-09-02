85d62a0eee7165aadfccf7544f82971aa2148f92
Indushree K L <Indushree K L@DESKTOP-MDJFMQN>
2026-09-02 10:27:44 +0530

    first commit

---
+++ TNSIF_TRAINING/src/org/tnsif/acce/c2tc/interview_pr_qsns1/String_Palindrome.java
@@ -0,0 +1,23 @@
+package org.tnsif.acce.c2tc.interview_pr_qsns1;
+import java.util.Scanner;
+
+public class String_Palindrome {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a string: ");
+		
+		String str = sc.nextLine();
+        String rev = "";
+
+		for (int i = str.length() - 1; i >= 0; i--) {
+		rev += str.charAt(i);
+	}
+		if (str.equals(rev))
+			System.out.println("Palindrome");
+		else
+			System.out.println("Not Palindrome");
+	}
+
+}
