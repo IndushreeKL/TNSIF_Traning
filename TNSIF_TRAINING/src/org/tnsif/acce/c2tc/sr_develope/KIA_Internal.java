df5b59478bae5b48d5850cbec7428427a7ef4f23
Indushree K L <Indushree K L@DESKTOP-MDJFMQN>
2026-09-02 11:28:42 +0530

    first commit

---
+++ TNSIF_TRAINING/src/org/tnsif/acce/c2tc/sr_develope/KIA_Internal.java
@@ -0,0 +1,14 @@
+package org.tnsif.acce.c2tc.sr_develope;
+
+
+import org.tnsif.acce.c2tc.jr_developer.*;
+public class KIA_Internal {
+
+	public static void main(String[] args) {
+		KIA_Airbags airbag=new KIA_Airbags();
+		airbag.disp_airbags();
+		
+
+	}
+
+}
