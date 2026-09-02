08bb3d3804ae3eef187d1587f166d195dd0ff594
Indushree K L <Indushree K L@DESKTOP-MDJFMQN>
2026-09-02 12:10:54 +0530

    first commit

---
+++ TNSIF_TRAINING/src/org/tnsif/acce/c2tc/userdefined_inputs/AdvancedScannerMethod.java
@@ -0,0 +1,30 @@
+package org.tnsif.acce.c2tc.userdefined_inputs;
+import java.util.Scanner;
+
+public class AdvancedScannerMethod {
+
+	public static void main(String[] args) {
+		
+
+		        Scanner sc = new Scanner(System.in);
+
+		        System.out.print("Enter any value: ");
+
+		        if (sc.hasNext()) {
+		            System.out.println("Input is available.");
+
+		            if (sc.hasNextInt()) {
+		                int num = sc.nextInt();
+		                System.out.println("You entered an integer: " + num);
+		            } else {
+		                String str = sc.next();
+		                System.out.println("You entered a string: " + str);
+		            }
+		        } else {
+		            System.out.println("No input available.");
+		        }
+
+		        sc.close();
+		    }
+
+}
